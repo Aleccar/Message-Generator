eab7a0b2a149a2dce128de9bd369bf8eafb2d7f9
Alec Strand <alexander@strand.it>
2024-12-27 17:32:45 +0100

    Created the functionality of the program with pre-set parameters

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Message Generator
 
-A Codecademy Portfolio Project to generate a random message each time a user runs the program.
+A **Codecademy Portfolio Project** to generate a random inspirational quote every time a user runs the program.
+
+
+This was made by *Alexander Strand* 
+
---
+++ index.js
@@ -0,0 +1,27 @@
+
+const inspirationalQuote = (name) => {
+    // Three different parts of the randomly generated inspirational quote
+    const startLine = ["Every journey begins with a single step, ", "Always work hard, ", "Don't take anything for granted, ", "Don't let fear stop you from doing what you want to do,", "Make peace with not being the best, ", "Never stop chasing what you want to do, ", "If you don't believe in it, nobody will, ", "You are only at the beginning of your journey, ", "If we never try, ", "No matter how hard the past is, ", "Write the story you want to be told, ", "Stand your ground in the face of adversity, ", "Sometimes what we need is some adversity, ", "Strive to be 1% better than you were yesterday, "]
+
+    const middleLine = ["we shall never succeed, ", "you can change your life, ", "failure is not fatal, ", "remember to breathe, ", "this is not the end, ", "the journey is only beginning, ", "if we chase results, ", "only you can define who you are, ", "life is too short to dwell on insecurities, ", "don't let other people write your script, ", "take it one step at a time, ", "if you work consistently enough, ", "you can't wait, ", "gain the strength you need for tomorrow, ", "ignite the fire of inspiration inside of yourself, ", "make it happen, "]
+
+    const endLine = ["you can do this!", "I believe in you!", "so keep going!", "and keep going!", "the strength is already inside you!", "you write your own script!", "you define who you are!", "we only fail when we stop trying!", "love yourself!", "be yourself!", "define yourself!", "nothing is impossible!", "anything is possible if you put your mind to it!", "just take another step!", "consistency is key!", "believe in yourself!", "look back and realise how far you've already come!", "failure is part of success!", "backwards is not the direction you're going!"]
+
+
+    const randomOne = Math.floor(Math.random() * startLine.length)
+    const randomTwo = Math.floor(Math.random() * middleLine.length)
+    const randomThree = Math.floor(Math.random() * endLine.length)
+
+    // Testing the different logic to make sure it works the way I intended.
+    // console.log(randomOne)
+    // console.log(randomTwo)
+    // console.log(randomThree)
+    // console.log(startLine.length)
+    // console.log(middleLine.length)
+    // console.log(endLine.length)
+
+    return `Here's some inspiration for you ${name}!\n\n${startLine[randomOne]}\n${middleLine[randomTwo]}\n${endLine[randomThree]}`
+}
+
+
+console.log(inspirationalQuote("Alec"))
